926e19406f4af654b57e0666078ff95f0ffd6390
narqu660i <cieslak994@icloud.com>
2025-07-17 19:10:19 +0200

    Stworzenie i ostylowanie sekcji #testimonials

--- script.js
+++ script.js
@@ -2,9 +2,36 @@
 
 const menuBtn = document.querySelector(".menu-btn");
 const line = document.querySelector(".line");
-const mainMenu = document.querySelector(".main-menu")
+const mainMenu = document.querySelector(".main-menu");
 
 menuBtn.addEventListener("click", () => {
-    line.classList.toggle("active");
-    mainMenu.classList.toggle("active");
-})
+  line.classList.toggle("active");
+  mainMenu.classList.toggle("active");
+});
+
+// === Testimonials ===
+
+const gap = 30;
+const container = document.querySelector(".testimonials-container");
+const testimonial = document.querySelector(".testimonial");
+
+document.querySelector(".button-right").addEventListener("click", function () {
+  const scrollAmount = testimonial.offsetWidth + gap;
+  const maxScrollLeft = container.scrollWidth - container.clientWidth;
+
+  if (container.scrollLeft + scrollAmount <= maxScrollLeft) {
+    container.scrollBy({ left: scrollAmount, behavior: "smooth" });
+  } else {
+    container.scrollTo({ left: maxScrollLeft, behavior: "smooth" });
+  }
+});
+
+document.querySelector(".button-left").addEventListener("click", function () {
+  const scrollAmount = testimonial.offsetWidth + gap;
+
+  if (container.scrollLeft - scrollAmount >= 0) {
+    container.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+  } else {
+    container.scrollTo({ left: 0, behavior: "smooth" });
+  }
+});
